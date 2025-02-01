981759fbbf772c1d2c61739fdcdb3638623fd010
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-02 01:38:23 +0600

    data added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { default: axios } = require("axios");
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -81,8 +82,8 @@ app.get("/rumor-detector", async (req, res) => {
 app.get("/generate-json", async (req, res) => {
     const prompt = req.query?.prompt;
     if (!prompt) {
-      res.send({ message: "please provide a prompt in query" });
-      return;
+        res.send({ message: "please provide a prompt in query" });
+        return;
     }
     const finalPrompt = `generate some data from this prompt ${prompt}  using this JSON schema:
    data = {'datatype': output}
@@ -91,7 +92,30 @@ app.get("/generate-json", async (req, res) => {
     const output = result.response.text().slice(7, -4);
     const jsonData = JSON.parse(output);
     res.send(jsonData);
-  });
+});
+
+app.get("/gen", async (req, res) => {
+    const prompt = req.query?.prompt;
+    if (!prompt) {
+        res.send({ message: "please provide a prompt in query" });
+        return;
+    }
+    const response = await axios.get(prompt, { responseType: "arraybuffer" });
+    const responseData = {
+        inlineData: {
+            data: Buffer.from(response.data).toString("base64"),
+            mimeType: "image/png",
+        },
+    };
+
+    const result = await model.generateContent([
+        "tell the detail of the image",
+        responseData,
+    ]);
+    console.log(result.response.text());
+
+    res.send({ detail: result.response.text() });
+});
 
 app.get("/test-ai", async (req, res) => {
     const prompt = req.query?.prompt;
